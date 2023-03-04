b032bf781f22c8e5914ed5f5c5e075e296504e50
Mark Chalenko <mark.chalenko@rndx2.com>
2023-03-04 20:53:01 +0300

    feat: added NATS in docker; fix docker issues

--- core/src/main.ts
+++ core/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: ['nats:4222'],
+        queue: 'default',
+      },
+    },
+  );
+  app.listen();
+  logger.log('Microservice A listening');
 }
 bootstrap();
--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'SERVICE_A',
+        transport: Transport.NATS,
+        options: {
+          servers: ['nats:4222'],
+          queue: 'default',
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
